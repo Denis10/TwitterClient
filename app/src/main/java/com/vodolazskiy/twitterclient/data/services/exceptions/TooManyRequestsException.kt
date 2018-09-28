3e5c6356ceb0dd0f8ba7015c7ce3b730e3bf17ff
Denis <denis.vodolazskiy@gmail.com>
2018-09-28 14:43:14 +0300

    Adds sealed class for network exceptions

--- app/src/main/java/com/vodolazskiy/twitterclient/data/services/exceptions/TooManyRequestsException.kt
+++
@@ -1,3 +0,0 @@
-package com.vodolazskiy.twitterclient.data.services.exceptions
-
-class TooManyRequestsException(detailMessage: String, throwable: Throwable) : NetworkException(detailMessage, throwable)
